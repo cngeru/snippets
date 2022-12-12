c0a9ab7617c013bf29544bef6be60687fe1b6792
chege <cn@igroove.ch>
2022-12-12 14:59:38 +0300

    added function

---
+++ 1/function.ts
@@ -0,0 +1,30 @@
+// 
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+import dayjs = require("dayjs");
+
+export const DeleteOldBookings = functions.pubsub.schedule('every day 00:00').timeZone('Africa/Nairobi').onRun(async(_context) => {
+  const timeUnix = dayjs().subtract(1,'day').unix() * 1000
+  const query = db.collection('bookings').where("timeInfo.startTimeStamp","<=",timeUnix).limit(400);
+  return new Promise((resolve, reject) => {
+    deleteQueryBatch(query, resolve).catch(reject);
+  });
+});
+
+async function deleteQueryBatch(query:any, resolve:any) {
+  const snapshot = await query.get();
+  const batchSize = snapshot.size;
+  console.log(`To delete ${batchSize}`);
+  if (batchSize === 0) {
+    resolve();
+    return;
+  }
+  const batch = db.batch();
+  snapshot.docs.forEach((doc: { ref: any; }) => {
+    batch.delete(doc.ref);
+  });
+  await batch.commit()
+  process.nextTick(() => {
+    deleteQueryBatch(query, resolve).catch((e)=>console.log(e));
+  });
+}
